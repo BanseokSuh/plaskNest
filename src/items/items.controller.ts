348fb9c07adce2f21c3e8256e1b8a2d8feb03f88
banseokSuh <still3028@gmail.com>
2021-08-23 11:15:00 +0900

    delete item

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { diskStorage } from 'multer';
@@ -8,23 +16,22 @@ import { ItemsService } from './items.service';
 @ApiTags('ITEMS')
 @Controller('api/items')
 export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
 
-	constructor(private readonly itemsService: ItemsService) { }
+  @Get()
+  getItems(@Query() query) {
+    return this.itemsService.getItems(query.limitCount);
+  }
 
-	@Get()
-	getItems(@Query() query) {
-		return this.itemsService.getItems(query.limitCount);
-	}
+  @ApiOperation({ summary: '상품 조회' })
+  @Get('orderby') // 순서매기기
+  getItemsOrdered(@Query() query) {
+    return this.itemsService.getItemsOrdered(query.limitCount, query.orderBy);
+  }
 
-	@ApiOperation({ summary: '상품 조회' })
-	@Get('orderby') // 순서매기기
-	getItemsOrdered(@Query() query) {
-		return this.itemsService.getItemsOrdered(query.limitCount, query.orderBy);
-	}
-
-	// 상품 등록
-	@ApiOperation({ summary: '상품 등록' })
-	@UseInterceptors(
+  // 상품 등록
+  @ApiOperation({ summary: '상품 등록' })
+  @UseInterceptors(
     FileInterceptor('itemImg', {
       storage: diskStorage({
         destination(req, file, cb) {
@@ -37,8 +44,23 @@ export class ItemsController {
       limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
     }),
   )
-	@Post('add')
-	addItem(@Body() body: AdditemRequestDto, @UploadedFile() file: Express.Multer.File) {
-		return this.itemsService.addItem(body.name, body.detail, body.price, body.access_token, file)
-	}
+  @Post('add')
+  addItem(
+    @Body() body: AdditemRequestDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return this.itemsService.addItem(
+      body.name,
+      body.detail,
+      body.price,
+      body.access_token,
+      file,
+    );
+  }
+
+  // 아이템 삭제
+  @Post('delete')
+  deleteItem(@Body() body) {
+    return this.itemsService.deleteItem(body.id);
+  }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -7,103 +7,118 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class ItemsService {
+  constructor(
+    @InjectRepository(Items) private itemsRepository: Repository<Items>,
+    private readonly usersService: UsersService,
+  ) {}
 
-	constructor(
-		@InjectRepository(Items) private itemsRepository: Repository<Items>,
-		private readonly usersService: UsersService
-	) { }
+  async getItems(limitCount: number) {
+    const items = await this.itemsRepository
+      .createQueryBuilder('items')
+      .limit(limitCount)
+      .getMany();
+    return items;
+  }
 
-	async getItems(limitCount: number) {
-		const items = await this.itemsRepository
-			.createQueryBuilder('items')
-			.limit(limitCount)
-			.getMany();
-		return items;
-	}
+  // 순서에 따른 조회
+  async getItemsOrdered(limitCount: number, orderBy: string) {
+    let orderedItems;
+    // 최신순, 낮은가격순, 높은가격순, 별점순
+    switch (orderBy) {
+      case 'newest':
+        orderedItems = await this.getItemsOrderByNewest(limitCount);
+        break;
 
-	// 순서에 따른 조회
-	async getItemsOrdered(limitCount: number, orderBy: string) {
-		let orderedItems;
-		// 최신순, 낮은가격순, 높은가격순, 별점순
-		switch (orderBy) {
-			case 'newest':
-				orderedItems = await this.getItemsOrderByNewest(limitCount);
-				break;
-			
-			case 'highest':
-				orderedItems = await this.getItemsOrderByHighest(limitCount);
-				break;
-			
-			case 'lowest':
-				orderedItems = await this.getItemsOrderByLowest(limitCount);
-				break;
-			
-			case 'starCount':
-				orderedItems = await this.getItemsOrderByStarCount(limitCount);
-				break;
-		}
+      case 'highest':
+        orderedItems = await this.getItemsOrderByHighest(limitCount);
+        break;
 
-		return orderedItems;
-	}
+      case 'lowest':
+        orderedItems = await this.getItemsOrderByLowest(limitCount);
+        break;
 
-	async getItemsOrderByNewest(limitCount) {
-		const items = await this.itemsRepository
-			.createQueryBuilder('items')
-			.orderBy('items.createdAt', 'DESC')
-			.take(limitCount)
-			.getMany();
-		return items;
-	}
-	async getItemsOrderByHighest(limitCount) {
-		const items = await this.itemsRepository
-			.createQueryBuilder('items')
-			.orderBy('items.price', 'DESC')
-			.take(limitCount)
-			.getMany();
-		return items;
-	}
-	async getItemsOrderByLowest(limitCount) {
-		const items = await this.itemsRepository
-			.createQueryBuilder('items')
-			.orderBy('items.price', 'ASC')
-			.take(limitCount)
-			.getMany();
-		return items;
-	}
-	async getItemsOrderByStarCount(limitCount) {
-		const items = await this.itemsRepository
-			.createQueryBuilder('items')
-			.orderBy('items.starCount', 'DESC')
-			.take(limitCount)
-			.getMany();
-		return items;
-	}
+      case 'starCount':
+        orderedItems = await this.getItemsOrderByStarCount(limitCount);
+        break;
+    }
 
+    return orderedItems;
+  }
 
+  async getItemsOrderByNewest(limitCount) {
+    const items = await this.itemsRepository
+      .createQueryBuilder('items')
+      .orderBy('items.createdAt', 'DESC')
+      .take(limitCount)
+      .getMany();
+    return items;
+  }
+  async getItemsOrderByHighest(limitCount) {
+    const items = await this.itemsRepository
+      .createQueryBuilder('items')
+      .orderBy('items.price', 'DESC')
+      .take(limitCount)
+      .getMany();
+    return items;
+  }
+  async getItemsOrderByLowest(limitCount) {
+    const items = await this.itemsRepository
+      .createQueryBuilder('items')
+      .orderBy('items.price', 'ASC')
+      .take(limitCount)
+      .getMany();
+    return items;
+  }
+  async getItemsOrderByStarCount(limitCount) {
+    const items = await this.itemsRepository
+      .createQueryBuilder('items')
+      .orderBy('items.starCount', 'DESC')
+      .take(limitCount)
+      .getMany();
+    return items;
+  }
 
-	async addItem(name: string, detail: string, price: number, access_token: string, file: Express.Multer.File) {
-		try {
-			const user = await this.usersService.getUserByAccessToken(access_token);
-			if (!user) {
-				throw new UnauthorizedException('유효하지 않은 토큰입니다.');
-			}
+  async addItem(
+    name: string,
+    detail: string,
+    price: number,
+    access_token: string,
+    file: Express.Multer.File,
+  ) {
+    try {
+      const user = await this.usersService.getUserByAccessToken(access_token);
+      if (!user) {
+        throw new UnauthorizedException('유효하지 않은 토큰입니다.');
+      }
 
-			const userId = user.id;
-			const itemObj: object = {
-				name,
-				detail,
-				price,
-				imgPath: `${process.env.SERVER_URL}/${file.path}`,
-				user: userId,
-			}
-	
-			await this.itemsRepository.save(itemObj)
-		} catch (e) {
-			throw e;
-		}
-	}
+      const userId = user.id;
+      const itemObj: object = {
+        name,
+        detail,
+        price,
+        imgPath: `${process.env.SERVER_URL}/${file.path}`,
+        user: userId,
+      };
 
-	deleteItem() {
-		
-	}
+      await this.itemsRepository.save(itemObj);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async deleteItem(target) {
+    const result = await this.itemsRepository.delete({
+      id: target,
+    });
+
+    if (result.affected !== 0) {
+      return {
+        message: '아이템이 삭제되었습니다.',
+      };
+    } else {
+      return {
+        message: '삭제된 아이템이 없습니다.',
+      };
+    }
+  }
 }
